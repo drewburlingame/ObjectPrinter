2f9fd60702f47735e327d8792a1e5cfb309538ee
drewburlingame <drewburlingame@gmail.com>
2012-01-04 08:24:09 -0800

    updating comments on DumpToLazyString to reflect what it truly does

--- src/ObjectPrinter/ObjectExtensions.cs
+++ src/ObjectPrinter/ObjectExtensions.cs
@@ -20,19 +20,28 @@
 			return new ObjectPrinter(obj, config).Print();
 		}
 
-		///<summary>Uses the ObjectPrinter to loop through the properties of the object, dumping them to a string.</summary>
+		///<summary>
+		/// Creates a delegate that will use the ObjectPrinter ont the obj when ToString() is called on the delegate.
+		/// Useful for delaying execution of the ObjectPrinter until absolutely needed. i.e. logging
+		/// </summary>
 		public static LazyStringDelegate DumpToLazyString(this object obj)
 		{
 			return new LazyStringDelegate(() => new ObjectPrinter(obj).Print());
 		}
 
-		///<summary>Uses the ObjectPrinter to loop through the properties of the object, dumping them to a string.</summary>
+		///<summary>
+		/// Creates a delegate that will use the ObjectPrinter ont the obj when ToString() is called on the delegate.
+		/// Useful for delaying execution of the ObjectPrinter until absolutely needed. i.e. logging
+		/// </summary>
 		public static LazyStringDelegate DumpToLazyString(this object obj, string tab, string newline)
 		{
 			return new LazyStringDelegate(() => new ObjectPrinter(obj, tab, newline).Print());
 		}
 
-		///<summary>Uses the ObjectPrinter to loop through the properties of the object, dumping them to a string.</summary>
+		///<summary>
+		/// Creates a delegate that will use the ObjectPrinter ont the obj when ToString() is called on the delegate.
+		/// Useful for delaying execution of the ObjectPrinter until absolutely needed. i.e. logging
+		/// </summary>
 		public static LazyStringDelegate DumpToLazyString(this object obj, IObjectPrinterConfig config)
 		{
 			return new LazyStringDelegate(() => new ObjectPrinter(obj, config).Print());
