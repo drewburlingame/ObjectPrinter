6141c8b1bab2228708846345b398132bb433578b
Drew Burlingame <drewburlingame@gmail.com>
2012-11-29 09:09:57 -0800

    consolidate Dump methods
     - use nullable params to reduce method signatures.

--- src/ObjectPrinter/ExceptionExtensions.cs
+++ src/ObjectPrinter/ExceptionExtensions.cs
@@ -1,5 +1,4 @@
 using System;
-using ObjectPrinter.Utilties;
 
 namespace ObjectPrinter
 {
@@ -9,33 +8,18 @@ namespace ObjectPrinter
 
 		///<summary>
 		/// Uses the ObjectPrinter to loop through the properties of the object, dumping them to a string.  
-		/// Includes special formatting to put all exception properties before the Exception.Data contents.
-		/// Caches object printer output in the exception so repetative calls are cheap, in case the output gets logged more than once.
-		/// </summary>
-		public static string DumpToString(this Exception e)
-        {
-            return GetCachedValue(e, e.Dump());
-		}
-
-		///<summary>
-		/// Uses the ObjectPrinter to loop through the properties of the object, dumping them to a string.  
         /// Includes special formatting to put all exception properties before the Exception.Data contents.
         /// Caches object printer output in the exception so repetative calls are cheap, in case the output gets logged more than once.
 		/// </summary>
-		public static string DumpToString(this Exception e, IObjectPrinterConfig config)
+		public static string DumpToString(this Exception e, IObjectPrinterConfig config = null)
 		{
-		    return GetCachedValue(e, e.Dump(config));
+		    if (!e.Data.Contains(CacheKey))
+		    {
+		        var output = e.Dump(config).ToString();
+		        e.Data[CacheKey] = output;
+		    }
+		    return (string) e.Data[CacheKey];
 		}
-
-	    private static string GetCachedValue(Exception e, LazyString ls)
-	    {
-	        if (!e.Data.Contains(CacheKey))
-	        {
-	            var output = ls.ToString();
-	            e.Data[CacheKey] = output;
-	        }
-	        return (string) e.Data[CacheKey];
-	    }
 
 	    public static void SetContext(this Exception e, string name, object context, bool ifNotSerializablePrintOnSerialize = false)
 		{
--- src/ObjectPrinter/ObjectExtensions.cs
+++ src/ObjectPrinter/ObjectExtensions.cs
@@ -5,33 +5,18 @@ namespace ObjectPrinter
 	public static class ObjectExtensions
 	{
 		///<summary>Uses the ObjectPrinter to loop through the properties of the object, dumping them to a string.</summary>
-		public static string DumpToString(this object obj)
+		public static string DumpToString(this object obj, IObjectPrinterConfig config = null)
 		{
-			return new ObjectPrinter(obj).PrintToString();
-		}
-
-		///<summary>Uses the ObjectPrinter to loop through the properties of the object, dumping them to a string.</summary>
-		public static string DumpToString(this object obj, IObjectPrinterConfig config)
-		{
-            return new ObjectPrinter(obj, config).PrintToString();
+		    return obj.Dump(config).ToString();
 		}
 
 		///<summary>
 		/// Creates a delegate that will use the ObjectPrinter ont the obj when ToString() is called on the delegate.
 		/// Useful for delaying execution of the ObjectPrinter until absolutely needed. i.e. logging
 		/// </summary>
-		public static LazyString Dump(this object obj)
+		public static LazyString Dump(this object obj, IObjectPrinterConfig config = null)
 		{
-            return new LazyString(() => new ObjectPrinter(obj).PrintToString());
-		}
-
-		///<summary>
-		/// Creates a delegate that will use the ObjectPrinter ont the obj when ToString() is called on the delegate.
-		/// Useful for delaying execution of the ObjectPrinter until absolutely needed. i.e. logging
-		/// </summary>
-		public static LazyString Dump(this object obj, IObjectPrinterConfig config)
-		{
-            return new LazyString(() => new ObjectPrinter(obj, config).PrintToString());
+            return new LazyString(() => new ObjectPrinter(obj, config ?? ObjectPrinter.GetDefaultContext()).PrintToString());
 		}
 	}
 }
