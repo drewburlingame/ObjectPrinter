6351f7c6b747eae4ac133a4aac249b7a9be4e455
Drew Burlingame <drewburlingame@gmail.com>
2012-11-29 09:11:16 -0800

    move NonSerializableWrapper to Utilities

--- src/ObjectPrinter/Utilties/NonSerializableWrapper.cs
+++ src/ObjectPrinter/Utilties/NonSerializableWrapper.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Runtime.Serialization;
 
-namespace ObjectPrinter
+namespace ObjectPrinter.Utilties
 {
 	[Serializable]
 	public class NonSerializableWrapper : ISerializable
