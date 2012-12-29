c24332c7c0138e73f3de3ae226531e728745ed17
Drew Burlingame <drewburlingame@gmail.com>
2012-12-29 15:50:59 -0800

    add comments to SetContext

--- src/ObjectPrinter/ExceptionExtensions.cs
+++ src/ObjectPrinter/ExceptionExtensions.cs
@@ -22,6 +22,17 @@ namespace ObjectPrinter
 		    return (string) e.Data[CacheKey];
 		}
 
+        /// <summary>
+        /// Adds context in the form of objects to the Data property of an exception.
+        /// This context will be printed with the exception.
+        /// </summary>
+        /// <param name="e">The exception to add context to</param>
+        /// <param name="name">The name to describe the context</param>
+        /// <param name="context">The context item.</param>
+        /// <param name="ifNotSerializablePrintOnSerialize">
+        /// When true, if the object is not serializable, the object will be dumped 
+        /// to string and added to the exception before serialization.  default=true
+        /// </param>
 	    public static void SetContext(this Exception e, string name, object context, bool ifNotSerializablePrintOnSerialize = true)
 		{
 			if (context != null && !context.GetType().IsSerializable)
