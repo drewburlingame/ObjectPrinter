e82ba7b4f6b8ec53e5300e9f168f99a49c1668d0
Drew Burlingame <drewburlingame@gmail.com>
2013-11-03 19:36:33 -0800

    support log4net 1.2.10, 1.2.11 & 1.2.12

---
+++ src/ObjectPrinter.Log4Net.v1212/Log4NetTypeInspector.cs
@@ -0,0 +1,23 @@
+using System;
+using ObjectPrinter.TypeInspectors;
+
+namespace ObjectPrinter.Log4Net
+{
+	/// <summary>
+	/// returns the ToString() represenation for every object in the "log4net*" namespaces, 
+	/// as log4net intended.  all bow to log4net.
+	/// </summary>
+	public class Log4NetTypeInspector : ToStringTypeInspector
+	{
+		public Log4NetTypeInspector()
+		{
+			ShouldInspectType = NamespaceIsLog4Net;
+		}
+
+		private static bool NamespaceIsLog4Net(object objectToInspect, Type typeOfObjectToInspect)
+		{
+			return typeOfObjectToInspect.Namespace != null
+			       && typeOfObjectToInspect.Namespace.StartsWith("log4net");
+		}
+	}
+}
