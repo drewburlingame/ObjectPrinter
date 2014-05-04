bfd9387de057358495a7b54c1ac6f5fdfd5100a5
drewburlingame <drewburlingame@gmail.com>
2014-05-04 07:45:40 -0700

    fix upload script

--- src/coverity.bat
+++ src/coverity.bat
@@ -1,5 +1,9 @@
 
-set output=c:\tmp\coverity\objectprinter
-cov-build --dir %output%\cov-int msbuild /t:Rebuild
-7z a -r %output%.zip %output%
-curl --form project=ObjectPrinter --form token=MowVRvISN77MeD3Lve66zQ --form email=drewburlingame@gmail.com --form file=@%output%.zip --form version=2.0.3 http://scan5.coverity.com/cgi-bin/upload.py
+set outputroot=c:\tmp\coverity\objectprinter
+set output=%outputroot%\cov-int
+set zip=%outputroot%.zip
+
+rm -fr %output%
+cov-build --dir %output% msbuild /t:Rebuild /p:Configuration=Release
+7z a -r %zip% %output%
+curl --form project=ObjectPrinter --form token=MowVRvISN77MeD3Lve66zQ --form email=drewburlingame@gmail.com --form file=@%zip% --form version=2.0.3 http://scan5.coverity.com/cgi-bin/upload.py
