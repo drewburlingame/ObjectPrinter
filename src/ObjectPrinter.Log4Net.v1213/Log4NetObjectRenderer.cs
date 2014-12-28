5961f864b0408d0cfc6681cb92f5902c1789f944
drewburlingame <drewburlingame@gmail.com>
2014-12-27 17:38:16 -0800

    support log4net 1.2.13

---
+++ src/ObjectPrinter.Log4Net.v1213/Log4NetObjectRenderer.cs
@@ -0,0 +1,44 @@
+using System.IO;
+using ObjectPrinter.Utilties;
+using log4net.ObjectRenderer;
+
+namespace ObjectPrinter.Log4Net
+{
+    /// <summary>
+    /// class used by log4net to render objects
+    /// </summary>
+	public class Log4NetObjectRenderer : IObjectRenderer
+	{
+		/// <summary></summary>
+		public void RenderObject(RendererMap rendererMap, object obj, TextWriter writer)
+		{
+			if (obj == null)
+			{
+				return;
+			}
+
+			var objAsString = obj as string;
+			if (obj is string)
+			{
+				writer.Write(objAsString);
+				return;
+			}
+
+			if (obj is LazyString)
+			{
+				writer.Write(obj.ToString());
+				return;
+			}
+
+			var ns = obj.GetType().Namespace;
+			if (!string.IsNullOrEmpty(ns) && (ns.StartsWith("log4net") || ns.StartsWith("Common.Logging")))
+			{
+				//if we get a log4net object, log4net will expect us to call ToString to get the format
+				writer.Write(obj.ToString());
+				return;
+			}
+
+            obj.DumpTo(writer);
+		}
+	}
+}
---
+++ src/ObjectPrinter.Log4Net.v1213/Log4NetTypeInspector.cs
@@ -0,0 +1,24 @@
+using System;
+using ObjectPrinter.TypeInspectors;
+
+namespace ObjectPrinter.Log4Net
+{
+	/// <summary>
+	/// returns the ToString() represenation for every object in the "log4net*" namespaces, 
+	/// as log4net intended.  all bow to log4net.
+	/// </summary>
+	public class Log4NetTypeInspector : ToStringTypeInspector
+    {
+        ///<summary></summary>
+		public Log4NetTypeInspector()
+		{
+			ShouldInspectType = NamespaceIsLog4Net;
+		}
+
+		private static bool NamespaceIsLog4Net(object objectToInspect, Type typeOfObjectToInspect)
+		{
+			return typeOfObjectToInspect.Namespace != null
+			       && typeOfObjectToInspect.Namespace.StartsWith("log4net");
+		}
+	}
+}
