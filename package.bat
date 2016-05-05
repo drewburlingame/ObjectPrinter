946c8b9bda74d0fc7b80b02447a4bf48eff272f3
drewburlingame <drewburlingame@gmail.com>
2016-05-05 16:53:25 -0700

    add v13 & v15 to package.bat

--- package.bat
+++ package.bat
@@ -13,11 +13,15 @@ SET OP=src\ObjectPrinter\ObjectPrinter.csproj
 SET L4J10=src\ObjectPrinter.Log4Net.v1210\ObjectPrinter.Log4Net.v1210.csproj
 SET L4J11=src\ObjectPrinter.Log4Net.v1211\ObjectPrinter.Log4Net.v1211.csproj
 SET L4J12=src\ObjectPrinter.Log4Net.v1212\ObjectPrinter.Log4Net.v1212.csproj
+SET L4J13=src\ObjectPrinter.Log4Net.v1213\ObjectPrinter.Log4Net.v1213.csproj
+SET L4J15=src\ObjectPrinter.Log4Net.v1215\ObjectPrinter.Log4Net.v1215.csproj
 
 %NUGET% pack %OP% -OutputDirectory nuget -Prop Configuration=Release
 %NUGET% pack %L4J10% -OutputDirectory nuget -Prop Configuration=Release
 %NUGET% pack %L4J11% -OutputDirectory nuget -Prop Configuration=Release
 %NUGET% pack %L4J12% -OutputDirectory nuget -Prop Configuration=Release
+%NUGET% pack %L4J13% -OutputDirectory nuget -Prop Configuration=Release
+%NUGET% pack %L4J15% -OutputDirectory nuget -Prop Configuration=Release
 
 ECHO.
 ECHO ending with:
