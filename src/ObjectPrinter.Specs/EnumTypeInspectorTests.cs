fad7d99263a37bd19434415082d25fafb6d8ef66
drewburlingame <drewburlingame@gmail.com>
2016-05-05 17:48:35 -0700

    remove MSpec.  it's breaking in appveyor

---
+++ src/ObjectPrinter.Specs/EnumTypeInspectorTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ObjectPrinter.Specs.EnumTypeInspectorSpecs
+{
+    [TestFixture]
+    public class EnumTypeInspectorTests
+    {
+        [Flags]
+        public enum ImFlagged
+        {
+            Val1, Val2, Val3
+        }
+        public enum ImNotFlagged
+        {
+            Val1, Val2, Val3
+        }
+
+        [Test]
+        public void Should_include_all_the_many_specified_values()
+        {
+            var flagged = ImFlagged.Val1 | ImFlagged.Val2;
+            var output = flagged.DumpToString();
+            //Console.Out.WriteLine(output);
+            output.Should().Contain("Val1");
+            output.Should().Contain("Val2");
+            output.Should().NotContain("Val3");
+            output.Should().NotContain("ObjectPrinter.Specs");
+            output.Should().Contain("ImFlagged");
+        }
+
+        [Test]
+        public void Should_be_only_the_specified_values()
+        {
+            var notFlagged = ImNotFlagged.Val2;
+            var output = notFlagged.DumpToString();
+            //Console.Out.WriteLine(output);
+            output.Should().Be("ImNotFlagged.Val2");
+        }
+    }
+}
